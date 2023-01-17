fea61aefcb3cbc17e72211595ddb5ab68fe85309
Rutuja <patil.rut@northeastern.edu>
2023-01-17 10:44:40 -0500

    First_Commit

---
+++ OneDrive/Documents/NetBeansProjects/GuiProject/src/Model/HelloWorld.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author rutuj
+ */
+public class HelloWorld {
+    public static int a=10;
+   public static double d=2.0;
+    public static float f=4;
+    public static String s="ABC";
+   public static  boolean b=true;
+    public static char c='a';
+    public static int n=20;
+    
+    public static int[] arr = new int[2];
+  
+    
+    
+    public static void main(String[] args)
+    {
+          arr[0] = 1;
+          arr[1] = 2;
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(arr[0]);
+         System.out.println(arr[1]);
+        
+}
+    
+}
+
+

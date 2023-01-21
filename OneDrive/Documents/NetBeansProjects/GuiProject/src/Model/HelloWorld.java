1177f458a4bb8fe6e4fa9847cf0cda9add71e541
Rutuja <patil.rut@northeastern.edu>
2023-01-21 13:11:10 -0500

    Assignment_1

--- OneDrive/Documents/NetBeansProjects/GuiProject/src/Model/HelloWorld.java
+++ OneDrive/Documents/NetBeansProjects/GuiProject/src/Model/HelloWorld.java
@@ -10,30 +10,24 @@ package Model;
  */
 public class HelloWorld {
     public static int a=10;
-   public static double d=2.0;
+    public static double d=2.0;
     public static float f=4;
     public static String s="ABC";
-   public static  boolean b=true;
-    public static char c='a';
-    public static int n=20;
-    
-    public static int[] arr = new int[2];
-  
-    
+    public static  boolean b=true;
+    public static char c='a'; 
+    public static int[] arr = {1,2};
     
     public static void main(String[] args)
     {
-          arr[0] = 1;
-          arr[1] = 2;
         System.out.println(a);
         System.out.println(d);
         System.out.println(f);
         System.out.println(s);
         System.out.println(b);
         System.out.println(c);
-        System.out.println(arr[0]);
-         System.out.println(arr[1]);
-        
+        for(int i=0; i<2;i++){            //printing the array
+            System.out.println(arr[i]);
+        }
 }
     
 }

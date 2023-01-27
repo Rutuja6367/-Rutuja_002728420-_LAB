24372f4333232633df189c7d22a1df4798f6904b
Rutuja <patil.rut@northeastern.edu>
2023-01-27 11:18:48 -0500

    UI_INFO

---
+++ AED_LAB/Student_Info/src/Model/Contact.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author rutuj
+ */
+public class Contact {
+    long phone;
+    String email;
+    
+    public Contact(){
+        
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+    
+    
+}
